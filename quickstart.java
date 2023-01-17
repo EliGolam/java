47a534955ee0330c2b81e8f1333b8a47b8e296cd
EliGolam <elistasibusiness@gmail.com>
2023-01-17 10:23:00 +0100

    First exercise

--- quickstart.java
+++ quickstart.java
@@ -1,5 +1,10 @@
 class QuickStart {
   public static void main (String[] args) {
     System.out.println("Hello World!");
+
+    int age = 22;
+    String name = "Britney";
+
+    System.out.printf("My name is %s, I'm %d", name, age);
   }
 }
